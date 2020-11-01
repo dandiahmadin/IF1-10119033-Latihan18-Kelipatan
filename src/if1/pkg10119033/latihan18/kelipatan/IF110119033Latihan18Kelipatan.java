4b16eaa823b399b99f93211fbe470232b8b9d091
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-11-01 09:17:06 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan18/kelipatan/IF110119033Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi kelipatan bilangan
+ **/
+
+public class IF110119033Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i;
+        
+        for (i = 3.5 ; i <= 35; i+=3.5) {
+            System.out.println(i);
+        }
+    }
+    
+}
